1b65a3f8b22cece52d24fc78d2bc5c0b21b2d306
samir perez <samir.perez10@gmail.com>
2023-08-30 20:59:55 -0600

    ejercicio grupal

---
+++ Condicionales.js
@@ -0,0 +1,28 @@
+const nombre = "Ada School";
+let edad = 123;
+let nota = 8;
+
+if(nombr === "Ada School"&& nota>=7)
+{
+    console.log("Hola"+ nombre +"su nota es ="+ nota);
+
+} else {
+    console.log("Nombre no existe");
+}
+
+const verdadero = "Positivo";
+const falso = "Negativo";
+let pos = 1;
+let neg = -1;
+
+if(verdadero === "Positivo") {
+    console.log("El numero" + pos + "es" + verdadero)
+} else{
+    console.log("El numero no es positivo")
+}
+
+if(falso === "Negativo") {
+    console.log("El numero" + pos + "es" + falso)
+} else{
+    console.log("El numero es positivo")
+}
